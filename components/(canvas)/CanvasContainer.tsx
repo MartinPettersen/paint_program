a18183efc92a0e9c9f974568d34e7dc00aba85ba
MartinPettersen <searchingtheskyfor@gmail.com>
2024-07-26 09:12:08 +0200

    allow users to use several different colors

--- components/(canvas)/CanvasContainer.tsx
+++ components/(canvas)/CanvasContainer.tsx
@@ -1,44 +1,62 @@
-import React, { useCallback, useRef, useState } from 'react'
-import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
-import { SkPath, TouchInfo, Skia, useTouchHandler, Canvas, Path } from "@shopify/react-native-skia";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import {
+  SkPath,
+  TouchInfo,
+  Skia,
+  useTouchHandler,
+  Canvas,
+  Path,
+} from "@shopify/react-native-skia";
 
 type Props = {
-  color: string,
-  strokeWidth: number,
-}
+  color: string;
+  strokeWidth: number;
+};
 
 type AdvancedPath = {
-  color: string,
-  strokeWidth: number,
-  path: SkPath,
-}
+  color: string;
+  strokeWidth: number;
+  path: SkPath;
+};
 
-const CanvasContainer = ({color, strokeWidth}: Props) => {
-
+const CanvasContainer = ({ color, strokeWidth }: Props) => {
   const [paths, setPaths] = useState<SkPath[]>([]);
+  const [paths2, setPaths2] = useState<AdvancedPath[]>([]);
 
+  const [pathColor, setPathColor] = useState<string>(color);
 
-  const touchStart = useCallback((touchInfo: TouchInfo) => {
-    setPaths((old) => {
-      const { x, y } = touchInfo;
-      const newPath = Skia.Path.Make();
-      newPath.moveTo(x, y);
-      return [...old, newPath];
-    })
-  },[])
+  const touchStart = useCallback(
+    (touchInfo: TouchInfo) => {
+      console.log(color, strokeWidth);
+      console.log(pathColor, strokeWidth);
+
+      setPaths2((old) => {
+        const { x, y } = touchInfo;
+        const newPath = Skia.Path.Make();
+        newPath.moveTo(x, y);
+        const newAdvancedPath = {
+          color: pathColor,
+          strokeWidth: strokeWidth,
+          path: newPath,
+        };
+        return [...old, newAdvancedPath];
+      });
+    },
+    [pathColor]
+  );
 
   const touching = useCallback((touchInfo: TouchInfo) => {
-    setPaths((currentPaths) => {
+    setPaths2((currentPaths) => {
       const { x, y } = touchInfo;
       const currentPath = currentPaths[currentPaths.length - 1];
-      const lastPoint = currentPath.getLastPt();
+      const lastPoint = currentPath.path.getLastPt();
       const xMid = (lastPoint.x + x) / 2;
       const yMid = (lastPoint.y + y) / 2;
-      currentPath.quadTo(lastPoint.x, lastPoint.y, xMid, yMid);
+      currentPath.path.quadTo(lastPoint.x, lastPoint.y, xMid, yMid);
       return [...currentPaths.slice(0, currentPaths.length - 1), currentPath];
     });
-  }, [])
-
+  }, []);
 
   const handleTouch = useTouchHandler(
     {
@@ -48,20 +66,25 @@ const CanvasContainer = ({color, strokeWidth}: Props) => {
     [touching, touchStart]
   );
 
+  useEffect(() => {
+    console.log("me called " + color);
+    setPathColor(color);
+  }, [color]);
+
   return (
     <Canvas style={styles.canvas} onTouch={handleTouch}>
-      {paths.map((path, index) => (
+      {paths2.map((path, index) => (
         <Path
           key={index}
-          path={path}
-          color={color}
+          path={path.path}
+          color={path.color}
           style={"stroke"}
-          strokeWidth={strokeWidth}
+          strokeWidth={path.strokeWidth}
         />
       ))}
     </Canvas>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   canvas: {
@@ -72,8 +95,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    width: "100%"
+    width: "100%",
   },
-})
+});
 
-export default CanvasContainer
+export default CanvasContainer;
